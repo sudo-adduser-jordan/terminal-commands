e12d1e0e8fcc9ebf2d34af04794329759f1c5946
jordan <108599131+sudo-adduser-jordan@users.noreply.github.com>
2023-11-06 10:38:26 -0800

    Update theme.config.tsx
    -update links
    -update footer

--- theme.config.tsx
+++ theme.config.tsx
@@ -4,14 +4,15 @@ import { DocsThemeConfig } from "nextra-theme-docs";
 const config: DocsThemeConfig = {
   logo: <span>--terminal-commands</span>,
   project: {
-    link: "https://github.com/shuding/nextra-docs-template",
+    link: "https://github.com/sudo-adduser-jordan/terminal-commands",
   },
   chat: {
     link: "https://discord.com",
   },
-  docsRepositoryBase: "https://github.com/shuding/nextra-docs-template",
+  docsRepositoryBase:
+    "https://github.com/sudo-adduser-jordan/terminal-commands",
   footer: {
-    text: "Nextra Docs Template",
+    text: "--terminal-commands",
   },
 };
 
